0b6798217d7c480e8ee6e0720ec282b0591c9878
sebaslade <sebaslade29@gmail.com>
2024-04-06 15:11:15 -0500

    Introduccion a Python

---
+++ Introduccion/01_variables.py
@@ -0,0 +1,24 @@
+# Variables
+variable_string = "Sebastián"
+print(variable_string)
+
+variable_int = 1
+print(variable_int)
+
+variable_boolean = True
+print(variable_boolean)
+
+my_int_to_str_variable = str(variable_int)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+# Concatenación de variables en un print
+print(variable_string, variable_int, variable_boolean)
+
+# funcion len
+print(len(variable_string))
+
+# Variables en una sola línea
+
+name, surname, alias, age = 'Sebastián','Laos','Zhare',19
+print("Me llamo:", name, surname,", mi edad es",age ,"y mi alias es",alias)
