c0885247b442dca087302996b3e03936b56e606b
sebaslade <sebaslade29@gmail.com>
2024-04-13 22:54:59 -0500

    Agregando pruebas en super set

---
+++ Introduccion/06_sets.py
@@ -0,0 +1,8 @@
+### Sets ###
+my_set = set()
+my_other_set = {} #-> en principio es un diccionario
+print(type(my_set))
+print(type(my_other_set))
+
+my_other_set = {"Sebastián", "Laos", 19}
+print(type(my_other_set))
