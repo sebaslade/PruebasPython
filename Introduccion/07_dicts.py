ef9b5bd9bdd529bc9c223742c5e24ef37ae07382
sebaslade <sebaslade29@gmail.com>
2024-04-14 22:04:13 -0500

    Pruebas de diccionarios

--- Introduccion/07_dicts.py
+++ Introduccion/07_dicts.py
@@ -12,3 +12,21 @@ my_other_dict = {
     "Edad": 19,
     1: "Python"
 }
+
+my_dict = {
+    "Nombre": "Sebastián",
+    "Apellido": "Laos",
+    "Edad": 19,
+    "Lenguages": {"Python","Java","Kotlin","JavaScript"}
+}
+
+print(my_dict["Nombre"])
+
+my_dict["Nombre"] = "Matias"
+print(my_dict["Nombre"])
+
+my_dict["Calle"] = "Av. X"
+print(my_dict)
+
+del my_dict["Calle"] #-> elimina un campo del diccionario
+print(my_dict)
